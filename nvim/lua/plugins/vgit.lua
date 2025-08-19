38d06b98a4f64b19c557c357510c8a5a11dd928c
Malico <yong@malico.me>
2025-08-19 17:12:09 +0100

    lazy git

--- nvim/lua/plugins/vgit.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "tanvirtin/vgit.nvim",
-    dependencies = { "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons" },
-    event = "VimEnter",
-    config = function()
-        require("vgit").setup()
-        vim.keymap.set("n", "<leader>gp", ":VGit project_diff_preview<CR>", { desc = "VGit Project Diff" })
-    end,
-}

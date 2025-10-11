068a454e776d7875842236f9db54c3ab8b08fa81
Malico <yong@malico.me>
2025-10-11 10:15:39 +0100

    refactor: formatting init

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,5 @@
-require('config.keymaps')
-require('config.options')
-require('config.lazy')
+require("config.keymaps")
+require("config.options")
+require("config.lazy")
 
-vim.cmd.colorscheme "onedark"
+vim.cmd.colorscheme("onedark")

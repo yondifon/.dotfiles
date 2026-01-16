47bf0c52ea7e6f9b1d7754b5126daa3573a5c23a
Malico <yong@malico.me>
2026-01-16 13:49:37 +0100

    revert the highlighter

--- nvim/after/queries/blade/highlights.scm
+++ nvim/after/queries/blade/highlights.scm
@@ -3,7 +3,6 @@
 (directive_end) @function
 (comment) @comment
 ((parameter) @include (#set! "priority" 110)) 
-((php_only) @include (#set! "priority" 110)) 
 ((bracket_start) @function (#set! "priority" 120)) 
 ((bracket_end) @function (#set! "priority" 120)) 
 (keyword) @function

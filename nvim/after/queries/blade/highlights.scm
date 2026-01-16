e35b113ff31fe7a8641d19d2576d3e05f4e37acc
Malico <yong@malico.me>
2026-01-16 13:55:01 +0100

    Revert "revert the highlighter"
    This reverts commit 47bf0c52ea7e6f9b1d7754b5126daa3573a5c23a.

--- nvim/after/queries/blade/highlights.scm
+++ nvim/after/queries/blade/highlights.scm
@@ -3,6 +3,7 @@
 (directive_end) @function
 (comment) @comment
 ((parameter) @include (#set! "priority" 110)) 
+((php_only) @include (#set! "priority" 110)) 
 ((bracket_start) @function (#set! "priority" 120)) 
 ((bracket_end) @function (#set! "priority" 120)) 
 (keyword) @function
